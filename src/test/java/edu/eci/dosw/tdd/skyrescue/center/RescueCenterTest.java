5e876048d4a766ae2ee26ca0aed8b65a96c9eb74
Sergio Buitrago <sergio.buitrago@mail.escuelaing.edu.co>
2026-09-15 17:36:04 -0500

    test: RED - muestra error si la mision no existe

--- src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
+++ src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -33,4 +33,17 @@ class RescueCenterTest {
             assertNotNull(e.getMessage());
             }
     }
+
+    @Test
+    void shouldThrowExceptionWhenCompletingNonExistentMission() {
+        RescueCenter center = new RescueCenter();
+
+        try {
+            center.completeMission("M-INEXISTENTE");
+            fail("Deberia haber lanzado IllegalArgumentException porque la mision no existe.");
+        } 
+        catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+        }
+    }
 }
