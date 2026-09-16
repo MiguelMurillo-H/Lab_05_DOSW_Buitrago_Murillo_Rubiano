6ec4a052430d919bac6000eefd297d1d28da2592
RubianoJ <juanita.rubiano-f@mail.escuelaing.edu.co>
2026-09-16 15:32:39 -0500

    testdeAddDrone: cubre casos de addDrone (dron nulo, id nulo, id en blanco, id duplicado)

--- src/main/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenter.java
+++ src/main/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenter.java
@@ -39,18 +39,14 @@ public class RescueCenter {
     public boolean addDrone(Drone drone) {
         if (drone == null) {
             return false;
-        }
-        else if (drone.getId() == null) {
+        } else if (drone.getId() == null) {
             return false;
-        }
-        else if (drone.getId().isBlank()) {
+        } else if (drone.getId().isBlank()) {
             return false;
-        }
-        else if (drones.containsKey(drone.getId())) {
+        } else if (drones.containsKey(drone.getId())) {
             return false;
-        }
-        else{
-        drones.put(drone.getId(), drone);
+        } else {
+            drones.put(drone.getId(), drone);
             return true;
         }
     }
@@ -74,8 +70,8 @@ public class RescueCenter {
      * - Valid resource but invalid state -> IllegalStateException.
      *
      * @param operatorId operator identifier.
-     * @param droneId drone identifier.
-     * @param location emergency location description.
+     * @param droneId    drone identifier.
+     * @param location   emergency location description.
      * @param distanceKm mission distance in kilometers.
      * @return created mission.
      */
--- src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
+++ src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -10,9 +10,50 @@ class RescueCenterTest {
     void shouldRegisterDroneWhenDataIsValid() {
         RescueCenter center = new RescueCenter();
         Drone drone = new Drone("D-01", "Phantom X", 15);
-        
+
         boolean result = center.addDrone(drone);
-        
+
         assertTrue(result);
+    }
+
+    @Test
+    void shouldNotRegisterDroneWhenDroneIsNull() {
+        RescueCenter center = new RescueCenter();
+
+        boolean result = center.addDrone(null);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldNotRegisterDroneWhenIdIsNull() {
+        RescueCenter center = new RescueCenter();
+        Drone drone = new Drone(null, "Phantom X", 15);
+
+        boolean result = center.addDrone(drone);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldNotRegisterDroneWhenIdIsBlank() {
+        RescueCenter center = new RescueCenter();
+        Drone drone = new Drone("   ", "Phantom X", 15);
+
+        boolean result = center.addDrone(drone);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldNotRegisterDroneWhenIdAlreadyExists() {
+        RescueCenter center = new RescueCenter();
+        Drone drone1 = new Drone("D-01", "Phantom X", 15);
+        Drone drone2 = new Drone("D-01", "Mavic Air", 10);
+
+        center.addDrone(drone1);
+        boolean result = center.addDrone(drone2);
+
+        assertFalse(result);
     }
 }
